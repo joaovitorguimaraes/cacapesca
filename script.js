dd8de5d057c62bae4a3f5f92d4ddd736f49a0b4a
joao vitor <102987009+joaovitorguimaraes@users.noreply.github.com>
2024-06-27 11:33:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,22 +8,22 @@ const perguntas = [
     {
       enunciado:
         "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-      alternativas: [
-        {
-          texto: "Isso é assustador!",
-          afirmacao: "afirmação",
-        },
-        {
-          texto: "Isso é maravilhoso!",
-          afirmacao: "afirmação",
-        },
-      ],
-    },
-    {
+     
       enunciado:
         "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
       alternativas: [
         {
+          texto:"Isso é assustador!",
+         afirmação: [
+          "No início ficou com medo do que essa tecnologia pode fazer.",
+          "Achou assustador pensar na velocidade na qual a tecnologia está avançando."
+         ],
+        },
+        {
+          texto:"Isso é maravilhoso!",
+          afirmacao:
+          "Quis saber como usar IA no seu dia a dia.",
+
           texto:
             "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
           afirmacao: "afirmação",
