2312aaf7d43b9bfa161cc77b3d4b687711fdd2d2
joao vitor <102987009+joaovitorguimaraes@users.noreply.github.com>
2024-08-01 10:46:58 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -2,7 +2,7 @@ export const perguntas = [
     {
       enunciado:
         "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-     
+
       enunciado:
         "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
       alternativas: [
